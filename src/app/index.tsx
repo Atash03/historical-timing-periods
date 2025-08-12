56643f48fa2bfb898c49761c989eef425aa46ea3
Atash03 <hugokarp12@gmail.com>
2025-08-13 01:06:46 +0500

    setup: add mock data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './globals.scss';
 import App from './App';
+import './globals.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
